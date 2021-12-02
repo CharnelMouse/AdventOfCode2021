8b9cd6d572b487911bb59947ab631c6f3f282bfb
CharnelMouse <markwebster204@yahoo.co.uk>
2021-12-02 10:55:04 +0000

    Simplify speed test

--- speed-test.r
+++ speed-test.r
@@ -4,16 +4,5 @@ times <- microbenchmark::microbenchmark(
   setup = expression(rm(list = ls()))
 )
 
-time_summary <- function(times, scale = 1) {
-  data.table::as.data.table(times)[
-    ,
-    .(
-      mean = mean(time/scale),
-      sd = sd(time/scale),
-      se = sd(time/scale)/.N,
-      `low (2.5%)` = quantile(time/scale, 0.025),
-      `high (97.5%)` = quantile(time/scale, 0.975)
-    ),
-    by = expr
-  ]
-}
+times
+print(times, "relative")
