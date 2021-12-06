884303f89b47074866d30d5907dfce5516c984cd
CharnelMouse <markwebster204@yahoo.co.uk>
2021-12-06 08:53:10 +0000

    Parse cells once for both parts of day 5

--- 05.r
+++ 05.r
@@ -5,28 +5,18 @@ lines <- scan(
   sep = ",",
   quiet = TRUE
 )
-
 straight <- which(lines$x0 == lines$x1 | lines$y0 == lines$y1)
 parse_line <- function(x0, y0, x1, y1) {
   paste(x0:x1, y0:y1, sep = ",")
 }
-
-straight_cells <- Map(
-  parse_line,
-  lines$x0[straight],
-  lines$y0[straight],
-  lines$x1[straight],
-  lines$y1[straight]
-) |>
-  unlist()
-length(unique(straight_cells[duplicated(straight_cells)])) # part one: 4421
-
 cells <- Map(
   parse_line,
   lines$x0,
   lines$y0,
   lines$x1,
   lines$y1
-) |>
-  unlist()
-length(unique(cells[duplicated(cells)])) # part two: 18674
+)
+straight_cells <- unlist(cells[straight])
+length(unique(straight_cells[duplicated(straight_cells)])) # part one: 4421
+all_cells <- unlist(cells)
+length(unique(all_cells[duplicated(all_cells)])) # part two: 18674
