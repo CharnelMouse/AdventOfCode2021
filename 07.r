21152bed7aad2ee9d73ab83aa41cdfa352a95a54
CharnelMouse <markwebster204@yahoo.co.uk>
2021-12-07 10:14:47 +0000

    Add some speed notes to day 7

--- 07.r
+++ 07.r
@@ -8,6 +8,13 @@ sum(abs(x - align_point)) # part one: 347449
 # the square distance and the absolute distance,
 # so we expect the optimal point m to lie
 # between the mean and the median.
+# We expect it to be much closer to the mean,
+# since the square distance has more influence,
+# so we could start at the mean and iterate
+# towards the median, breaking when the cost
+# begins to increase. However, in R this is
+# slower than calculating all the costs in
+# the interval.
 mn <- mean(x)
 range_min <- floor(min(mn, align_point))
 range_max <- ceiling(max(mn, align_point))
